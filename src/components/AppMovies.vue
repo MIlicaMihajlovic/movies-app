0fb24f4f21ab95951e331b04d61863f7c966c7cb
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-12-04 19:25:28 +0100

    Add movies service

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,12 +1,32 @@
 <template>
     <div>
+        <ul class="list-unstyled">
+            <li v-for="(movie, index) in movies" :key="index">
 
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
+
+import { movies } from '../service/movies.js'
+
 export default {
-    
+    data() {
+        return {
+            movies: []
+        }
+    },
+
+    beforeRouteEnter(to,from,next) {
+        movies.getAll()
+        .then(response => {
+            next(vm => {
+                vm.movies = response.data;
+            })
+        })
+    }
 }
 </script>
 
