ac06e60a89bf3bc6fd65cb33eb8cee0592e2df88
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-12-04 20:26:49 +0100

    Add movie list and movie row

--- src/components/AppMovies.vue
+++ src/components/AppMovies.vue
@@ -1,18 +1,26 @@
 <template>
     <div>
+        
         <ul class="list-unstyled">
             <li v-for="(movie, index) in movies" :key="index">
-
+                <movie-row :movie="movie"></movie-row>
             </li>
         </ul>
+        
     </div>
 </template>
 
 <script>
 
 import { movies } from '../service/movies.js'
+import MovieRow from './MovieRow.vue'
 
 export default {
+
+    components: {
+        MovieRow
+    },
+
     data() {
         return {
             movies: []
---
+++ src/components/MovieRow.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+        <h3>{{ movie.title }}</h3>
+        <p>{{ movie.director }}</p>
+        <img :src="movie.imageUrl"/>
+        <p>{{ movie.relaseDate }}</p>
+        <p>{{ movie.genre }}</p>
+        <p>{{ movie.duration }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    
+    props: ['movie']
+}
+</script>
+
